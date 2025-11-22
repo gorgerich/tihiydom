2073dceb7293b712b146f88db48b480bd65b53d8
gorgerich <pachuliya.a@gmail.com>
2025-11-22 22:17:52 +0300

    Update StepperWorkflow.tsx

--- ui/StepperWorkflow.tsx
+++ ui/StepperWorkflow.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { Stepper } from './Stepper';
+import Stepper from "./Stepper";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
