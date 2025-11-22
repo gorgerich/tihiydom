d5907b95e8b830759d9bf32b85fef2d4dd0a4250
gorgerich <pachuliya.a@gmail.com>
2025-11-22 16:11:20 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../ui/App";
+import App from "./ui/App";
+import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
